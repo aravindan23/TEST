563e2623d2a2a0d57cb4ede49c941c8a46138d35
aravindan23 <Aravindansankar97@gmail.com>
2019-03-28 00:05:36 +0530

    committed

--- maven-release-plugin-example/src/main/java/com/test/HelloController.java
+++ maven-release-plugin-example/src/main/java/com/test/HelloController.java
@@ -5,5 +5,6 @@ public class HelloController {
 	public static void main(String[] args) {
 System.out.print("HELLOController");
 		System.out.print("HELLOController2");
+		System.out.print("HELLOController3");
 	}
 }
