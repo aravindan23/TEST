505a2a74034fc78b04a3191bdb70789b8b04a1a1
aravindan23 <Aravindansankar97@gmail.com>
2019-04-19 12:21:16 +0530

    updated

--- maven-release-plugin-example/src/main/java/com/test/HelloController.java
+++ maven-release-plugin-example/src/main/java/com/test/HelloController.java
@@ -1,11 +1,18 @@
 package com.test;
-
+import java.util.*;
 public class HelloController {
 
 	public static void main(String[] args) {
 System.out.print("HELLOController");
+		Scanner sc=new Scanner(System.in);
 		System.out.print("HELLOController2");
 		System.out.print("HELLOController3");
 		System.out.print("HELLOController4");
+		System.out.print("HELLOController5");
+		System.out.print("HELLOController6");
+		System.out.print("HELLOController7");
+		System.out.print("HELLOController8");
+		System.out.print("HELLOController9");
+		System.out.print("HELLOController10");
 	}
 }
