2ce8f0e2fce0d86544c2b4c3776c328d9c14c859
aravindan23 <Aravindansankar97@gmail.com>
2019-04-20 15:08:56 +0530

    update

--- maven-release-plugin-example/src/main/java/com/test/HelloController.java
+++ maven-release-plugin-example/src/main/java/com/test/HelloController.java
@@ -13,15 +13,6 @@ System.out.print("HELLOController");
 		System.out.print("HELLOController5");
 		System.out.print("HELLOController6");
 		System.out.print("HELLOController7");
-		System.out.print("HELLOController8");
-		System.out.print("HELLOController9");
-		System.out.print("HELLOController10");
-		System.out.print("HELLOController11");
-		System.out.print("HELLOController12");
-		System.out.print("HELLOController13");
-		System.out.print("HELLOController14");
-                 System.out.print("HELLOController15");
-                System.out.print("HELLOController16");
 		System.out.print("print"+a);
 		System.out.print("print"+b);
 	}
