9d9170b6037b47d813e0b634daf44fdc1667b5d5
test <example.com>
2019-04-20 06:02:12 -0400

    update

--- maven-release-plugin-example/src/main/java/com/test/HelloController.java
+++ maven-release-plugin-example/src/main/java/com/test/HelloController.java
@@ -20,6 +20,8 @@ System.out.print("HELLOController");
 		System.out.print("HELLOController12");
 		System.out.print("HELLOController13");
 		System.out.print("HELLOController14");
+                System.out.print("HELLOController15");
+                System.out.print("HELLOController16");
 		System.out.print("print"+a);
 		System.out.print("print"+b);
 	}
